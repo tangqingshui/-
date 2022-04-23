52d62be53ed8fec29bd6386bd853fabf9b8765ad
fyjg <912718496@qq.com>
2022-04-23 23:39:18 +0800

    1

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,112 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    {{formDefaultData}}
+    <comForm>
+      123
+    </comForm>
+    {{formData}}
   </div>
 </template>
+
+<script setup>
+import {
+  h,
+  ref,
+  reactive,
+  onMounted,
+  resolveComponent,
+  defineEmits,
+  watch,
+  defineProps,
+  toRefs,
+  useSlots
+} from 'vue'
+ 
+const props = defineProps({
+  readonly: {
+    type: Boolean,
+    default: () => false,
+  },
+  isUpdate: {
+    type: Boolean,
+    default: () => false,
+  },
+  listOptions: {
+    type: Object,
+    default: () => {},
+  },
+  formDefaultData: {
+    type: Object,
+    default: () => {},
+  },
+  controls: {
+    type: Object,
+    default: () => [],
+  },
+  formRules: {
+    type: Object,
+    default: () => {},
+  },
+})
+const emit = defineEmits(['comInput'])
+const {
+  readonly,
+  isUpdate,
+  listOptions,
+  formDefaultData,
+  controls,
+  formRules,
+} = toRefs(props)
+const form = ref()
+const formData = reactive({
+  a: 5,
+  b: 2,
+})
+const comForm = () => {
+  let result = null
+  result = h(
+    resolveComponent('el-form'),
+    { ref: form, model: formData },
+    () => {
+    return controls.value.map((item) => {
+      return baseFormItemBuild(item)
+    })
+  },
+  )
+  return result
+}
+const baseFormItemBuild = (obj) => {
+  let result = null
+  result = h(
+    resolveComponent('el-form-item'),
+    {
+      style: { width: '400px' },
+      prop: obj.prop,
+      required: obj.required,
+      label: obj.label,
+      labelWidth: obj.labelWidth ? item.labelWidth : '100px',
+    },
+    () => [baseInput(obj)]
+  )
+  return result
+}
+const baseInput = (obj) => {
+  let result = null
+  result = h(resolveComponent('el-input'), {
+    modelValue: formData[obj.prop],
+    onInput: (val) => comInput(val, obj.prop),
+    ...obj.props,
+  })
+  return result
+}
+const submit = () => {
+  form.value.validate((e) => {
+    console.log(e)
+  })
+}
+const comInput = (val, prop) => {
+  formData[prop] = val
+  emit('comInput', { prop, val })
+}
+console.log(comForm)
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <com-form :controls="controls">
+      <template #aa>
+        22
+      </template>
+    </com-form>
+    <com-form :form-default-data="formDefaultData"
+              @comInput="comInput"
+              :controls="controls"></com-form>
+    <div @click="changDefaultData">cc</div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import { reactive, ref } from '@vue/reactivity'
+import comForm from './About.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+const controls = reactive([
+  { prop: 'a', label: '试验1', required: true, props: { readonly: true } },
+  { prop: 'b', label: '试验2', required: false },
+  { prop: 'c', label: '试验2', required: false },
+  { prop: 'd', label: '试验2', required: false },
+  { prop: 'e', label: '试验2', required: false },
+  { prop: 'f', label: '试验2', required: false },
+])
+const formDefaultData = reactive({ required: true })
+const changDefaultData = () => {
+  controls[0].label = 'aaa'
+  console.log(controls)
+}
+const comInput = (e) => {
+  console.log(e)
 }
 </script>
